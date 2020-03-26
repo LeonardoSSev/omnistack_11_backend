4661390b988ab7570639660d2378c8ba3930dde0
leonardossev <leonardossev@gmail.com>
2020-03-26 20:45:11 -0300

    chore(package): desinstala mongoose

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const routes = require('./routes');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 const port = process.env.APP_PORT | 3333;
-
-mongoose.connect(process.env.DB_CONNECTION, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-});
 
 app.use(cors());
 app.use(express.json());
