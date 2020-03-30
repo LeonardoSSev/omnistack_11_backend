65d26655634bd076558ffec68df7e792cfe731f9
leonardossev <leonardossev@gmail.com>
2020-03-30 20:55:27 -0300

    feat(routes): adiciona rota de cadastro de ong

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const OngController = require('../controllers/OngController');
 
 const routes = new Router();
+
+routes.post('/ongs', OngController.store);
 
 module.exports = routes;
