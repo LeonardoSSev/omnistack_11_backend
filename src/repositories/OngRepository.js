406b8bccd95953250186956b839cde500bdbc0b8
leonardossev <leonardossev@gmail.com>
2020-03-30 21:24:32 -0300

    feat(repositories): adiciona findAll e findById

--- src/repositories/OngRepository.js
+++ src/repositories/OngRepository.js
@@ -1,6 +1,17 @@
 const connection = require('../database/connection');
 
 module.exports = {
+  findById(ongId) {
+    return connection('ongs')
+      .select('*')
+      .where({ id: ongId })
+      .first();
+  },
+
+  findAll() {
+    return connection('ongs').select('*');
+  },
+
   store({ongId, name, email, whatsapp, city, uf }) {
     return connection('ongs').insert({
       name,
