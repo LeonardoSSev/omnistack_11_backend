42ed25a8c17a823544fef28e32ba5273a0254838
leonardossev <leonardossev@gmail.com>
2020-03-30 21:25:54 -0300

    feat(controllers): adiciona index

--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -1,6 +1,12 @@
 const OngService = require('../services/OngService');
 
 module.exports = {
+  async index(request, response) {
+    const ongs = await OngService.findAll();
+
+    return response.send(ongs);
+  },
+
   async store(request, response) {
     const ongId = await OngService.storeOng(request.body);
 
