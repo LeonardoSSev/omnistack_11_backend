b2c0c6d64e5e506649f2af02c231090ee3ac9ed7
leonardossev <leonardossev@gmail.com>
2020-03-30 21:25:37 -0300

    feat(services): adiciona findAll e findOng

--- src/services/OngService.js
+++ src/services/OngService.js
@@ -2,6 +2,14 @@ const crypto = require('crypto');
 const OngRepository = require('../repositories/OngRepository');
 
 module.exports = {
+  async findOng(ongId) {
+    return await OngRepository.findById(ongId);
+  },
+  
+  async findAll() {
+    return await OngRepository.findAll();
+  },
+
   async storeOng({ name, email, whatsapp, city, uf }) {
     const ongId = crypto.randomBytes(4).toString('HEX');
 
