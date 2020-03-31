5f9f6d222d0708955575783c2056c3673087c913
leonardossev <leonardossev@gmail.com>
2020-03-30 21:25:00 -0300

    feat(routes): adiciona listagem protegida de ongs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
+const AuthMiddleware = require('../middlewares/AuthMiddleware');
 const OngController = require('../controllers/OngController');
 
 const routes = new Router();
 
+routes.get('/ongs', AuthMiddleware.verifyOng, OngController.index);
 routes.post('/ongs', OngController.store);
 
 module.exports = routes;
