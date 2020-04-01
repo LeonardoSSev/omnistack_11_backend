977a6caf8d6901c72812d04c2b1abee88062a85d
leonardossev <leonardossev@gmail.com>
2020-03-31 22:24:12 -0300

    change(services): retorna random id gerado

--- src/services/OngService.js
+++ src/services/OngService.js
@@ -13,8 +13,8 @@ module.exports = {
   async storeOng({ name, email, whatsapp, city, uf }) {
     const ongId = crypto.randomBytes(4).toString('HEX');
 
-    const [id] = await OngRepository.store({ongId, name, email, whatsapp, city, uf});
+    await OngRepository.store({ongId, name, email, whatsapp, city, uf});
 
-    return id;
+    return ongId;
   }
 }
